abca1d52dd402e323b2a69d02b75646fdaee492f
PJ Dev <prabhjotdev@live.com>
2021-04-18 12:14:56 -0400

    Added error handling to Registration screen

--- graphic-artem/src/Screens/Registration/GeneralRegistration.js
+++ graphic-artem/src/Screens/Registration/GeneralRegistration.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { useForm } from 'react-hook-form';
 import history from '../../history';
 import './cssFiles/GeneralRegistration.css';
 
-class GeneralRegistration extends Component {
-    render(){
+const GeneralRegistration = ()=> {
+    const { register, handleSubmit, formState:{errors} } = useForm();
+    const onSubmit = (data) => {
+        console.log(data)
+        history.push('/Home')
+    };  
         return(
             <div class=".col-md-4">
                 <div class="card">
@@ -11,15 +16,17 @@ class GeneralRegistration extends Component {
                     <header class="mb-4">
                         <h4 class="card-title">Sign up</h4>
                     </header>
-                    <form class="block-register">
+                    <form onSubmit={handleSubmit(onSubmit)} class="block-register">
                     
                     <div class="form-group form-row">
                         <label class="col-md-3 col-form-label">Full name</label>
                         <div class="col">
-                        <input type="text" class="form-control" placeholder="First name"/>
+                        <input type="text" class="form-control" placeholder="First name" {...register("firstname", {required: true, maxLength:80})}/>
+                        {errors.firstname && "First name is required"}
                         </div>
                         <div class="col">
-                        <input type="text" class="form-control" placeholder="Last name"/>
+                        <input type="text" class="form-control" placeholder="Last name" {...register("lastName", {required: true, maxLength:100})}/>
+                        {errors.lastName && "Last name is required"}
                         </div>
                     </div>
 
@@ -28,7 +35,8 @@ class GeneralRegistration extends Component {
                         <label class="col-md-3 col-form-label">Phone</label>
                         <div class="col-9">
                         <div class="input-group">
-                            <input name="" class="form-control" placeholder="Phone number" type="text"/>
+                            <input name="" class="form-control" placeholder="Phone number" type="text" {...register("phone", {required: true, minLength:6, maxLength:12})}/>
+                            {(errors.phone) && "Phone number is required or enter correct amount of digits"}
                         </div>
                         </div>
                     </div>
@@ -36,7 +44,8 @@ class GeneralRegistration extends Component {
                     <div class="form-group form-row">
                         <label class="col-md-3 col-form-label">Email</label>
                         <div class="col-sm-9">
-                        <input type="email" class="form-control" placeholder="Ex. john@gmail.com"/>
+                        <input type="text" class="form-control" placeholder="Ex. john@gmail.com" {...register("email", {required: true})}/>
+                        {errors.email && "email is required or does not require the matching example"}
                         </div>
                     </div>
 
@@ -44,36 +53,41 @@ class GeneralRegistration extends Component {
                     <div class="form-group form-row">
                         <label class="col-md-3 col-form-label">Address</label>
                         <div class="col">
-                        <input type="text" class="form-control" placeholder="City"/>
+                        <input type="text" class="form-control" placeholder="City" {...register("city", {required: true, maxLength:80})}/>
+                        {errors.city && "City is required"}
+                        <input type="text" class="form-control" placeholder="Province/State" {...register("province", {required: true})}/>
+                        {errors.province && "Province is required"}
                         </div>
                         <div class="col">
-                        <select class="custom-select form-control">
+                        <select class="custom-select form-control" {...register("country", {required: true})}>
                             <option>Country</option>
                             <option>United states</option>
                             <option>Canada</option>
                         </select>
+                        {errors.country && "Country is required"}
                         </div>
                     </div>
 
                     <div class="form-group form-row">
                         <label class="col-md-3"> </label>
                         <div class="col">
-                        <input type="text" class="form-control" placeholder="Street, Building"/>
+                        <input type="text" class="form-control" placeholder="Street, Building" {...register("street", {required: true, maxLength:80})}/>
+                        {errors.street && "Street address is required"}
                         </div>
                     </div>
 
                     <div class="form-group form-row">
                         <label class="col-md-3 col-form-label">Password</label>
                         <div class="col-6">
-                        <input type="password" class="form-control mb-3" placeholder="Create password"/>
-                        <input type="password" class="form-control" placeholder="Repeat password"/>
+                        <input type="password" class="form-control mb-3" placeholder="Create password" {...register("password", {required: true, minLength:8, maxLength:24})} />
+                        {errors.password && "Password is required"}
                         </div>
                     </div>
 
                     <div class="form-group form-row">
                         <div class="col-sm-9 offset-sm-3">
                             <label class="custom-control custom-checkbox">
-                            <input type="checkbox" class="custom-control-input" id="customCheck1"/>
+                            <input type="checkbox" class="custom-control-input" id="customCheck1" {...register("terms", {required: true})}/>
                             <div class="custom-control-label" for="customCheck1">I agree with <a href="#" target="_blank">terms and conditions</a></div>
                             </label>
 
@@ -82,7 +96,8 @@ class GeneralRegistration extends Component {
 
                     <div class="form-row mb-2">
                         <div class="col-sm-9 offset-sm-3">
-                        <button type="submit" onClick={()=>history.push('/Home')} class="btn btn-primary">Register</button>
+                        <button type="submit" class="btn btn-primary">Register</button> 
+                        {/* onClick={()=>history.push('/Home')} */}
                         </div>
                     </div>
 
@@ -92,7 +107,7 @@ class GeneralRegistration extends Component {
                 </div>
             </div>
         );
-    }
+    // }
 }
 
 export default GeneralRegistration;
--- graphic-artem/src/Screens/Registration/ProfessionalRegisteration.js
+++ graphic-artem/src/Screens/Registration/ProfessionalRegisteration.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import { useForm } from 'react-hook-form';
 import history from '../../history';
 import './cssFiles/ProfessionalRegisteration.css';
 
-class ProfessionalRegisteration extends Component {
-    render(){
+const ProfessionalRegisteration = ()=> {
+    const { register, handleSubmit, formState:{errors} } = useForm();
+    const onSubmit = (data) => {
+        console.log(data)
+        history.push('/Home')
+    };  
         return(
             <div class=".col-md-4">
                 <div class="card">
@@ -11,13 +16,14 @@ class ProfessionalRegisteration extends Component {
                     <header class="mb-4">
                         <h4 class="card-title">Sign up</h4>
                     </header>
-                    <form class="block-register">
+                    <form onSubmit={handleSubmit(onSubmit)} class="block-register">
                     
                     <div class="form-group form-row">
                         <label class="col-md-3 col-form-label">Business Name</label>
                         <div class="col-9">
                             <div class="input-group">
-                                <input name="" class="form-control" placeholder="Business Name" type="text"/>
+                                <input name="" class="form-control" placeholder="Business Name" type="text" {...register("businessName", {required: true, maxLength:80})}/>
+                                {errors.businessName && "Business name is required"}
                             </div>
                         </div>
                     </div>
@@ -25,10 +31,12 @@ class ProfessionalRegisteration extends Component {
                     <div class="form-group form-row">
                         <label class="col-md-3 col-form-label">Full name</label>
                         <div class="col">
-                        <input type="text" class="form-control" placeholder="First name"/>
+                        <input type="text" class="form-control" placeholder="First name" {...register("firstName", {required: true, maxLength:80})}/>
+                        {errors.firstName && "First name is required"}
                         </div>
                         <div class="col">
-                        <input type="text" class="form-control" placeholder="Last name"/>
+                        <input type="text" class="form-control" placeholder="Last name" {...register("lastName", {required: true, maxLength:80})}/>
+                        {errors.lastName && "Last name is required"}
                         </div>
                     </div>
 
@@ -37,7 +45,8 @@ class ProfessionalRegisteration extends Component {
                         <label class="col-md-3 col-form-label">Phone</label>
                         <div class="col-9">
                         <div class="input-group">
-                            <input name="" class="form-control" placeholder="Phone number" type="text"/>
+                            <input name="" class="form-control" placeholder="Phone number" type="text" {...register("phone", {required: true, maxLength:80})}/>
+                            {errors.phone && "Phone number is required"}
                         </div>
                         </div>
                     </div>
@@ -45,7 +54,8 @@ class ProfessionalRegisteration extends Component {
                     <div class="form-group form-row">
                         <label class="col-md-3 col-form-label">Email</label>
                         <div class="col-sm-9">
-                        <input type="email" class="form-control" placeholder="Ex. john@gmail.com"/>
+                        <input type="email" class="form-control" placeholder="Ex. john@gmail.com" {...register("email", {required: true, maxLength:80})}/>
+                        {errors.email && "Email is required"}
                         </div>
                     </div>
 
@@ -53,57 +63,64 @@ class ProfessionalRegisteration extends Component {
                     <div class="form-group form-row">
                         <label class="col-md-3 col-form-label">Address</label>
                         <div class="col">
-                        <input type="text" class="form-control" placeholder="City"/>
+                        <input type="text" class="form-control" placeholder="City" {...register("city", {required: true, maxLength:80})}/>
+                        {errors.city && "City is required"}
                         </div>
                         <div class="col">
-                        <select class="custom-select form-control">
+                        <select class="custom-select form-control" {...register("country", {required: true})}>
                             <option>Country</option>
                             <option>United states</option>
                             <option>Canada</option>
                         </select>
+                        {errors.country && "Please select a country"}
                         </div>
                     </div>
 
                     <div class="form-group form-row">
                         <label class="col-md-3"> </label>
                         <div class="col">
-                        <input type="text" class="form-control" placeholder="Street, Building"/>
+                        <input type="text" class="form-control" placeholder="Street, Building" {...register("street", {required: true})}/>
+                        {errors.street && "Street name is required"}
                         </div>
                     </div>
 
                     <div class="form-group form-row">
                         <label class="col-md-3 col-form-label">Password</label>
                         <div class="col-6">
-                        <input type="password" class="form-control mb-3" placeholder="Create password"/>
-                        <input type="password" class="form-control" placeholder="Repeat password"/>
+                        <input type="password" class="form-control mb-3" placeholder="Create password" {...register("password", {required: true, minLength: 8, maxLength:24})}/>
+                        {errors.password && "Password is required"}
                         </div>
                     </div>
 
                     <div class="form-group form-row">
                         <label class="col-md-3">Credit Card Name</label>
                         <div class="col">
-                        <input type="text" class="form-control" placeholder="Name on Card"/>
+                        <input type="text" class="form-control" placeholder="Name on Card" {...register("cardName", {required: true, maxLength:30})}/>
+                        {errors.cardName && "Credit Card name is required"}
                         </div>
                     </div>
 
                     <div class="form-group form-row">
                         <label class="col-md-3">Credit Card Number</label>
                         <div class="col">
-                        <input type="text" class="form-control" placeholder="Number on Card"/>
+                        <input type="text" class="form-control" placeholder="Number on Card" {...register("cardNumber", {required: true, maxLength:16})}/>
+                        {errors.cardNumber && "Card Number is required"}
                         </div>
                     </div>
 
                     <div class="form-group form-row">
                         <label class="col-md-3">Credit Card Expiry</label>
                         <div class="col">
-                        <input type="text" maxLength="4" class="form-control" placeholder="MM/YY"/>
+                        <input type="text" maxLength="4" class="form-control" placeholder="MM/YY" {...register("exp", {required: true, maxLength:4})}/>
+                        {errors.exp && "Expiry of card is required"}
                         </div>
                     </div>
 
                     <div class="form-group form-row">
                         <label class="col-md-3">Credit Card CV</label>
                         <div class="col">
-                        <input type="text" maxLength="3" class="form-control" placeholder="CV"/>
+                        <input type="text" maxLength="3" class="form-control" placeholder="CV" {...register("cv", {required: true, maxLength:3})}/>
+                        {errors.cv && "CV on card is required"}
                         </div>
                     </div>
 
@@ -119,7 +136,7 @@ class ProfessionalRegisteration extends Component {
 
                     <div class="form-row mb-2">
                         <div class="col-sm-9 offset-sm-1">
-                        <button type="submit" class="btn btn-primary" onClick={()=>history.push('/Home')}>Register</button>
+                        <button type="submit" class="btn btn-primary">Register</button>
                         </div>
                     </div>
 
@@ -129,7 +146,7 @@ class ProfessionalRegisteration extends Component {
                 </div>
             </div>
         );
-    }
+    
 }
 
 export default ProfessionalRegisteration;
